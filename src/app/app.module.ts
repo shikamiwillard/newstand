7de6cf5a09ad736cedfba0877e989cf8f424bc4b
shikamiwillard <willardshikami@gmail.com>
2019-03-07 16:36:09 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './modules/homepage/components/search/search.component';
+import { HomeComponent } from './shared/components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
