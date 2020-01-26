d2f6f41548077b0aaa06440db34886467a8a0690
Willard Shikami <willardshikami@gmail.com>
2020-01-26 22:21:33 +0300

    ui: complete landing page ui elements

--- lib/features/news/pages/launch_screen.dart
+++ lib/features/news/pages/launch_screen.dart
@@ -8,14 +8,15 @@ class Launch extends StatelessWidget{
         child: Container(
           child: Center(
             child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              LaunchImage(),
               Container(
-                child: LaunchImage()
-                ),
-              Container(
+                padding: EdgeInsets.all(30.0),
                 child: Text('Welcome to Newstand a news app with no clutter', 
-                style: TextStyle(fontFamily: 'OpenSans', fontSize: 18.0),),)
-              
+                textAlign: TextAlign.center,
+                style: TextStyle(fontFamily: 'OpenSans', fontSize: 24.0, fontWeight: FontWeight.w500),),),
+                LaunchButton(),
             ],
           ),
         ),
@@ -27,20 +28,26 @@ class Launch extends StatelessWidget{
 class LaunchImage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-  AssetImage assetImage = AssetImage('newstand/assets/images/logo.png');
-  Image launchImageAsset = Image(image: assetImage);
-    return Container(child: launchImageAsset,);
+  AssetImage assetImage = AssetImage('assets/images/logo.png');
+  Image image = Image(image: assetImage, height: 200.0, width: 200.0,);
+    return Container(child: image,);
   }
 }
 
 class LaunchButton extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      child: Text('Get started'),
+    return Container(
+      padding: EdgeInsets.only(top: 20.0),
+      child: RaisedButton(
+        color: Color(0xFF4B8E8D),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      padding: EdgeInsets.only(left: 120.0, right: 120.0, top: 15.0, bottom: 15.0),
+      child: Text('Get started', style: TextStyle(fontFamily: 'OpenSans', fontSize: 22.0, fontWeight: FontWeight.w500, color: Colors.white),),
       onPressed: (){
-
+    
       },
+    )
     );
   }
   
