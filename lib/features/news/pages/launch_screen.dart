8930c3a272ecbb1e009767515550991c174ce649
Willard Shikami <willardshikami@gmail.com>
2020-01-29 21:32:42 +0300

    gitignore files

--- lib/features/news/pages/launch_screen.dart
+++ lib/features/news/pages/launch_screen.dart
@@ -12,7 +12,7 @@ class Launch extends StatelessWidget{
             children: <Widget>[
               LaunchImage(),
               Container(
-                padding: EdgeInsets.all(30.0),
+                padding: EdgeInsets.only(right: 40.0, left: 40.0),
                 child: Text('Welcome to Newstand a news app with no clutter', 
                 textAlign: TextAlign.center,
                 style: TextStyle(fontFamily: 'OpenSans', fontSize: 24.0, fontWeight: FontWeight.w500),),),
@@ -29,7 +29,7 @@ class LaunchImage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
   AssetImage assetImage = AssetImage('assets/images/logo.png');
-  Image image = Image(image: assetImage, height: 200.0, width: 200.0,);
+  Image image = Image(image: assetImage, height: 180.0, width: 180.0,);
     return Container(child: image,);
   }
 }
@@ -38,14 +38,14 @@ class LaunchButton extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 20.0),
+      padding: EdgeInsets.only(top: 30.0),
       child: RaisedButton(
         color: Color(0xFF4B8E8D),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      padding: EdgeInsets.only(left: 120.0, right: 120.0, top: 15.0, bottom: 15.0),
-      child: Text('Get started', style: TextStyle(fontFamily: 'OpenSans', fontSize: 22.0, fontWeight: FontWeight.w500, color: Colors.white),),
+      padding: EdgeInsets.only(left: 115.0, right: 115.0, top: 12.0, bottom: 12.0),
+      child: Text('Get started', style: TextStyle(fontFamily: 'OpenSans', fontSize: 18.0, fontWeight: FontWeight.w500, color: Colors.white),),
       onPressed: (){
-    
+        
       },
     )
     );
